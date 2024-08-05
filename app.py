195aeae00bc94ba2c837d4cc9335e429664996be
tameszaza <wasan55411@gmail.com>
2024-08-05 17:30:43 +0700

    fix app route

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
